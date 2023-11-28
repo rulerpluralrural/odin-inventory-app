f3ea5389e0a65df33337b4cd24e21a2ee959cf55
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2023-11-28 21:18:57 +0800

    Added category mongoose schema model

---
+++ models/Category.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const CategorySchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, "Please provide a name"],
+		maxLength: 50,
+	},
+	description: {
+		type: String,
+		required: [true, "Please provide a description"],
+		maxLength: 200,
+	},
+});
+
+CategorySchema.virtual("url").get(function () {
+	return `/category/${this._id}`;
+});
+
+module.exports = mongoose.model("Category", CategorySchema);
