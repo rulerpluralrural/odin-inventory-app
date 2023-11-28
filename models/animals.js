ea67d0000af1a0d8bc7d95ba1347bc340ad3776b
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2023-11-28 21:14:07 +0800

    Added animal mongoose schema model

---
+++ models/animals.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const AnimalSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Please provide a name"],
+			maxLength: 50,
+		},
+		description: {
+			type: String,
+			required: [true, "Please provide a description"],
+			maxLength: 200,
+		},
+		status: {
+			type: String,
+			enum: ["least concern", "vulnerable", "endangered", "extinct"],
+			default: "least concern",
+		},
+		imgUrl: {
+			type: String,
+			unique: true,
+		},
+		category: {
+			type: mongoose.Types.ObjectId,
+			ref: "Category",
+			required: [true, "Please provide a category"],
+		},
+	},
+	{ timestamps: true }
+);
+
+AnimalSchema.path("imageURL").validate((val) => {
+	urlRegex =
+		/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/;
+	return urlRegex.test(val);
+}, "Invalid URL");
+
+AnimalSchema.virtual("url").get(function () {
+	return `animal/${this._id}`;
+});
+
+module.exports = mongoose.model("Animal", AnimalSchema);
