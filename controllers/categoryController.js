f4c20410407fd42dc3b94cdce3247b33153e5404
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2023-12-02 20:39:09 +0800

    Finished controllers to handle form GET and POST

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,7 +2,9 @@ const Animal = require("../models/Animal");
 const Category = require("../models/Category");
 const { StatusCodes } = require("http-status-codes");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
+// Display Home Page
 exports.index = asyncHandler(async (req, res, next) => {
 	const [getAllAnimals, getAllCategories] = await Promise.all([
 		Animal.find().exec(),
@@ -16,6 +18,7 @@ exports.index = asyncHandler(async (req, res, next) => {
 	});
 });
 
+// Display detail page for a specific category
 exports.category_details = asyncHandler(async (req, res, next) => {
 	const [category, getAllAnimals] = await Promise.all([
 		Category.findById(req.params.id),
@@ -29,17 +32,57 @@ exports.category_details = asyncHandler(async (req, res, next) => {
 	});
 });
 
+// Display category form on GET
 exports.category_create_get = asyncHandler(async (req, res, next) => {
 	res.render("category/category_form", {
-		title: "Wildlife Info",
+		title: "Create a category",
 	});
 });
 
-exports.category_create_post = asyncHandler(async (req, res, next) => {
-	const category = await Category.create(req.body);
+// Handle category create form
+exports.category_create_post = [
+	// Validate and sanitize fields
+	body("category_name")
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage("Category name must be specified")
+		.isAlphanumeric()
+		.withMessage("Category name has non-alpanumeric characters"),
+	body("description")
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage("Description must be specified"),
 
-	res.status(StatusCodes.CREATED).json({ category });
-});
+	// Process request after validation and sanitation
+	asyncHandler(async (req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+
+		// Create new category with escaped and trimmed data
+		const category = new Category({
+			name: req.body.category_name,
+			description: req.body.description,
+		});
+
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values/errors messages.
+			res.render("category/category_form", {
+				title: "Create Category",
+				category: category,
+				errors: errors.array(),
+			});
+			return;
+		} else {
+			// Data form is valid.
+			//Save category
+			await category.save();
+			// Redirect to new category record.
+			res.redirect(category.url);
+		}
+	}),
+];
 
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
 	res.send("GET Delete Category");
